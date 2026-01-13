1e01499c9328e04c22f5eb856c314549e5b2ee6f
pamix2004 <pawelmichon2004@gmail.com>
2026-01-13 20:46:59 +0100

    Changed GatewayFilter to be reactive

--- cloud-gateway/demo/src/main/java/com/cloudGateway/demo/filter/AuthenticationFilter.java
+++ cloud-gateway/demo/src/main/java/com/cloudGateway/demo/filter/AuthenticationFilter.java
@@ -5,13 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpCookie;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
@@ -19,73 +16,59 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
     @Autowired
     private RouteValidator validator;
 
-    public AuthenticationFilter(){
+    private final WebClient webClient;
+
+    public AuthenticationFilter(WebClient.Builder webClientBuilder) {
         super(Config.class);
+        this.webClient = webClientBuilder.baseUrl("http://localhost:8085").build();
     }
-    @Autowired
-    private RestTemplate template;
 
     @Override
     public GatewayFilter apply(Config config) {
+        System.out.println("Gateway teraz filtruje");
         return (exchange, chain) -> {
+
 
             if (!validator.isSecured.test(exchange.getRequest())) {
                 return chain.filter(exchange);
             }
 
-            // get auth_token cookie
-            HttpCookie authCookie = exchange.getRequest()
-                    .getCookies()
-                    .getFirst("auth_token");
-
+            HttpCookie authCookie = exchange.getRequest().getCookies().getFirst("auth_token");
             if (authCookie == null) {
-                System.out.println("Authorization token is missing");
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
             }
 
             String token = authCookie.getValue();
 
-            try {
-                ResponseEntity<TokenValidationResponse> response =
-                        template.getForEntity(
-                                "http://localhost:8085/auth/validateToken?token={token}",
-                                TokenValidationResponse.class,
-                                token
-                        );
+            return webClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/auth/validateToken")
+                            .queryParam("token", token)
+                            .build())
+                    .retrieve()
+                    .bodyToMono(TokenValidationResponse.class)
+                    .flatMap(body -> {
+                        if (body == null || !body.isValid()) {
+                            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                            return exchange.getResponse().setComplete();
+                        }
 
-                TokenValidationResponse body = response.getBody();
+                        String userId = String.valueOf(body.getUserId());
 
-                if (body == null || !body.isValid()) {
-                    throw new RuntimeException("Invalid token");
-                }
+                        ServerHttpRequest mutatedRequest = exchange.getRequest()
+                                .mutate()
+                                .header("X-User-Id", userId)
+                                .build();
 
-                int userId = body.getUserId();
-                System.out.println("User authenticated, userId=" + userId);
-
-                // add header to downstream request
-                ServerHttpRequest mutatedRequest = exchange.getRequest()
-                        .mutate()
-                        .header("X-User-Id", String.valueOf(userId))
-                        .build();
-
-                ServerWebExchange mutatedExchange = exchange
-                        .mutate()
-                        .request(mutatedRequest)
-                        .build();
-
-                return chain.filter(mutatedExchange);
-
-            } catch (Exception e) {
-                System.out.println("invalid access...!");
-                System.out.println("un authorized access to application");
-                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                return exchange.getResponse().setComplete();
-            }
+                        return chain.filter(exchange.mutate().request(mutatedRequest).build());
+                    })
+                    .onErrorResume(err -> {
+                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                        return exchange.getResponse().setComplete();
+                    });
         };
     }
 
-    public static class Config{
-
-    }
+    public static class Config {}
 }
